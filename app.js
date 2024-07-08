87d3d3dd004656c09de00710ad088bad7950a41f
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-07-08 12:16:57 -0300

    Se prueban bucles contadores funcionando

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // Actividad 1: Contador del 1 al 10
-
+/*
 console.log("Contador del 1 al 10:");
 let contador1 = 1;
 while (contador1 <= 10) {
     console.log(contador1);
     contador1++;
 }
-/*
+
 // Actividad 2: Contador regresivo de 10 a 0
 console.log("\nContador regresivo de 10 a 0:");
 let contador2 = 10;
@@ -14,7 +14,7 @@ while (contador2 >= 0) {
     console.log(contador2);
     contador2--;
 }
-
+*/
 // Actividad 3: Cuenta progresiva hasta un número dado por el usuario
 let numeroUsuario = prompt("Por favor, ingresa un número:");
 numeroUsuario = Number(numeroUsuario);
@@ -27,4 +27,4 @@ if (isNaN(numeroUsuario)) {
         console.log(contador3);
         contador3++;
     }
-}*/
+}
